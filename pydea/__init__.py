27c90e54daaf46ae594cc61772c0f6fe1de8e355
Manuel Palomo Ramón <manuel.palomo@aitana.es>
2018-01-29 16:57:19 +0100

    Changed folder structure

---
+++ pydea/__init__.py
@@ -0,0 +1,2 @@
+from . import Tweet
+from . import TwitterSearcher
---
+++ pydea/main.py
@@ -0,0 +1,9 @@
+#from twitter_searcher import TwitterSearcher, write_list_to_file
+from twitter_searcher import TwitterSearcher,write_list_to_file
+def main():
+    searcher = TwitterSearcher()
+    write_list_to_file(searcher.simple_search(1), 'test.txt')
+
+
+if __name__ == "__main__":
+    main()
